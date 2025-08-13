dfc5edac9f573f6ebc34ad6085fdf655b4b042a5
Alex <puyaalexxx@gmail.com>
2025-08-13 12:47:01 +0300

    added product model properties

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAILABLE_PRODUCTS = 'available';
+    const UNAVAILABLE_PRODUCTS = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailable()
+    {
+        return $this->status === self::AVAILABLE_PRODUCTS;
+    }
+
+    public function isUnAvailable()
+    {
+        return $this->status === self::UNAVAILABLE_PRODUCTS;
+    }
 }
