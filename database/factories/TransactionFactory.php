9cd08fb3284a9891901ead89815dcd2978a80ab9
Alex <puyaalexxx@gmail.com>
2025-08-13 15:26:07 +0300

    added transaction factory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Seller;
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $seller = Seller::has('products')->get()->random();
+        $buyer = User::all()->except($seller->id)->random();
+
+        return [
+            'quantity' => fake()->numberBetween(1, 3),
+            'buyer_id' => $buyer->id,
+            'product_id' => $seller->products->random()->id,
+        ];
+    }
+}
