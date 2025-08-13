c2a56bccc5ca936fe2ed58e637a96cddb668a541
Alex <puyaalexxx@gmail.com>
2025-08-13 14:52:29 +0300

    added category_product migration

---
+++ database/migrations/2025_08_13_114942_category_product_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->integer('category_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->foreignId('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_product');
+    }
+};
